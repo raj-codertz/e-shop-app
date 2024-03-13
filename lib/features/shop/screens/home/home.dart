9a017e26be78fd5a7a3768e70367b764019d9920
rajabujshabani <rajabujshabani@gmail.com>
2024-03-13 11:47:55 +0300

    complete category widget

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:raj_store_app/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:raj_store_app/utils/constants/colors.dart';
+import 'package:raj_store_app/utils/constants/image_strings.dart';
 import 'package:raj_store_app/utils/constants/sizes.dart';
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 import '../../../../common/widgets/custom_shapes/containers/search_container.dart';
@@ -10,7 +12,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -19,25 +21,100 @@ class HomeScreen extends StatelessWidget {
                 child: Column(
               children: [
                 // Appbar
-                THomeAppBar(),
-                SizedBox(height: TSizes.spaceBtwSections),
+                const THomeAppBar(),
+                const SizedBox(height: TSizes.spaceBtwSections),
 
                 //   Searchbar
-                TSearchContainer(text: 'Search in Store'),
-                SizedBox(height: TSizes.spaceBtwSections),
+                const TSearchContainer(text: 'Search in Store'),
+                const SizedBox(height: TSizes.spaceBtwSections),
 
                 //   Categories
                 Padding(
-                  padding: EdgeInsets.only(left: TSizes.defaultSpace),
+                  padding: const EdgeInsets.only(left: TSizes.defaultSpace),
                   child: Column(
                     children: [
-                      TSectionHeading(
-                          title: 'Popular Categories', showActionButton: false)
+                      // Heading
+                      const TSectionHeading(
+                          title: 'Popular Categories',
+                          showActionButton: false,
+                          textColor: TColors.white),
+                      const SizedBox(height: TSizes.spaceBtwItems),
+
+                      //   Categories
+                      SizedBox(
+                        height: 80,
+                        child: ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: 6,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (_, index) {
+                              return const TVerticalImageText();
+                            }),
+                      )
                     ],
                   ),
                 )
               ],
             ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TVerticalImageText extends StatelessWidget {
+  const TVerticalImageText({
+    super.key,
+    required this.image,
+    required this.title,
+    this.textColor = TColors.white,
+    this.backgroundColor,
+    this.onTap,
+  });
+
+  final String image, title;
+  final Color textColor;
+  final Color? backgroundColor;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.only(right: TSizes.spaceBtwItems),
+        child: Column(
+          children: [
+            // Circular Icon
+            Container(
+              width: 56,
+              height: 56,
+              padding: const EdgeInsets.all(TSizes.sm),
+              decoration: BoxDecoration(
+                  color: backgroundColor,
+                  borderRadius: BorderRadius.circular(100)),
+              child: Center(
+                child: Image(
+                    image: AssetImage(image),
+                    fit: BoxFit.cover,
+                    color: TColors.dark),
+              ),
+            ),
+
+            //   Text
+            const SizedBox(height: TSizes.spaceBtwItems / 2),
+            SizedBox(
+                width: 55,
+                child: Text(
+                  title,
+                  style: Theme.of(context)
+                      .textTheme
+                      .labelMedium!
+                      .apply(color: textColor),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ))
           ],
         ),
       ),

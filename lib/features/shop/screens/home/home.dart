11c8800278cc665f51c903acd48dcf4fcb8a46ac
rajabujshabani <rajabujshabani@gmail.com>
2024-03-13 14:55:20 +0300

    create a carousel slider with dots

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:raj_store_app/features/shop/screens/home/widgets/home_appbar.dart';
 import 'package:raj_store_app/features/shop/screens/home/widgets/home_categories.dart';
 import 'package:raj_store_app/utils/constants/colors.dart';
+import 'package:raj_store_app/utils/constants/image_strings.dart';
 import 'package:raj_store_app/utils/constants/sizes.dart';
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 import '../../../../common/widgets/custom_shapes/containers/search_container.dart';
@@ -12,12 +13,12 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             // Header
-            TPrimaryHeaderContainer(
+            const TPrimaryHeaderContainer(
                 child: Column(
               children: [
                 // Appbar
@@ -46,7 +47,16 @@ class HomeScreen extends StatelessWidget {
                   ),
                 )
               ],
-            ))
+            )),
+
+            //   Body
+            Container(
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(TSizes.md)),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(TSizes.md),
+                  child: Image(image: AssetImage(TImages.promoBanner3))),
+            )
           ],
         ),
       ),
